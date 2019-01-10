229f71520002b8839ba89295090700f99fe5cd14
djarvis <dave.jarvis@gmail.com>
2019-01-09 19:56:54 -0800

    Force users to set username using the dash u argument.

--- github-email.sh
+++ github-email.sh
@@ -35,16 +35,16 @@ FILE_GITHUB_TOKEN=$HOME/.ghtoken
 ARG_QUIET=true
 
 # Forces user's repository
-ARG_REPOSITORY=
+unset ARG_REPOSITORY
 
 # API token value from -t or $FILE_GITHUB_TOKEN
-ARG_GITHUB_TOKEN=
+unset ARG_GITHUB_TOKEN
 
 # GitHub username to look up
-ARG_USERNAME=
+unset ARG_USERNAME
 
 # Set to anything to show help and exit
-ARG_HELP=
+unset ARG_HELP
 
 # -----------------------------------------------------------------------------
 # Script starts here
@@ -52,22 +52,25 @@ ARG_HELP=
 main() {
   parse_commandline $@
 
-  if [ ! -z $ARG_HELP ]; then
+  if [ ! -z "$ARG_HELP" ]; then
     show_usage
   fi
 
-  # Save the GitHub token to a file
-  if [ ! -z $ARG_GITHUB_TOKEN ]; then
+  # Save the GitHub token to a file, if one was parsed on the command line
+  if [ ! -z "$ARG_GITHUB_TOKEN" ]; then
     echo $ARG_GITHUB_TOKEN > $FILE_GITHUB_TOKEN
+  elif [ ! -z "$GITHUB_TOKEN" ]; then
+    # Read the standard GITHUB_TOKEN environment variable
+    ARG_GITHUB_TOKEN=$GITHUB_TOKEN
   fi
 
-  # Read the GitHub token from a file (if greater than zero bytes)
-  if [ -s $FILE_GITHUB_TOKEN ]; then
+  # Read the GitHub token from a file (if it is greater than zero bytes)
+  if [ -s "$FILE_GITHUB_TOKEN" ]; then
     ARG_GITHUB_TOKEN=$(cat $FILE_GITHUB_TOKEN)
   fi
 
   # Stop if no user name is given
-  if [ -z $ARG_USERNAME ]; then
+  if [ -z "$ARG_USERNAME" ]; then
     show_usage
   fi
 
@@ -79,10 +82,10 @@ main() {
   CMD="curl $ARG_QUIET"
 
   # Force token usage
-  if [ -z $ARG_GITHUB_TOKEN ]; then
-    warning "When run for the first time, generate a GitHub API token using:\n"
-		warning "    $SCRIPT_NAME -t [TOKEN]\n"
-    warning "See README.md for further instructions."
+  if [ -z "$ARG_GITHUB_TOKEN" ]; then
+    warning "When run for the first time, provide a GitHub API token using:\n"
+		warning "    $SCRIPT_NAME -u username -t [TOKEN]\n"
+    warning "See README.md for instructions on generating a token."
     exit 1
   else
 		heading 'GitHub'
@@ -103,7 +106,7 @@ main() {
     | sort -u
 
   heading 'Recent Repository Activity'
-  if [ -z $ARG_REPOSITORY ]; then
+  if [ -z "$ARG_REPOSITORY" ]; then
     # Get first repository if not specified
     ARG_REPOSITORY="$($CMD "$API_GITHUB/users/$ARG_USERNAME/repos?type=owner&sort=updated" \
       | sed -nE 's#^.*"name": "([^"]+)",.*$#\1#p' \
@@ -131,7 +134,7 @@ parse_commandline() {
       # Skip argument and value
       shift && shift
       ;;
-      -h|-?|--help)
+      -h|-\?|--help)
       ARG_HELP="true"
 			shift
       ;;
@@ -151,13 +154,10 @@ parse_commandline() {
       ARG_USERNAME="$2"
       shift && shift
       ;;
-      # Assume username, repository, and token.
       *)
-      ARG_USERNAME=$1
-      ARG_REPOSITORY=$2
-      ARG_GITHUB_TOKEN=$3
+      ARG_USERNAME="$1"
       # Skip arguments
-      shift && shift && shift
+      shift
       ;;
     esac
   done
@@ -165,7 +165,8 @@ parse_commandline() {
 }
 
 show_usage() {
-	printf "Usage: $SCRIPT_NAME username [repository]\n" >&2
+	printf "Usage: $SCRIPT_NAME -u username " >&2
+  printf "[-r repository] [-t token] [-q false]\n" >&2
 	exit 1
 }
 
